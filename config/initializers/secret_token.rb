b64be09140f765a7218cbad72c5419983a2a70cc
Clayton Jordan <clayjchicago@gmail.com>
2014-11-12 21:11:45 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IMDbClone::Application.config.secret_key_base = 'cdd3ddc3689dc2032894eda5a17b19163a4b2f40aecf37d97c771e62191c7c8a2f4339fe4e4fca8840e7c64b5579065d737c1463815c9f78ad743e45d7762f19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IMDbClone::Application.config.session_store :cookie_store, key: '_IMDbClone_session'
