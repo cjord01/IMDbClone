884a60caef75bb357207138d296018175b205097
Clayton Jordan <clayjchicago@gmail.com>
2015-01-21 11:28:58 -0600

    Fixed controller names

--- app/controllers/filmmakers_controller.rb
+++ app/controllers/filmmakers_controller.rb
@@ -1,10 +1,13 @@
-class FilmmakerController < ApplicationController
+class FilmmakersController < ApplicationController
 	def index
 		@filmmakers = Filmmaker.all.order("Name ASC").paginate(:page => params[:page], :per_page => 36)
 		@filmmaker = Filmmaker.new
 	end
 
 	def show
+	end
 
+	def new
+		@filmmaker = Filmmaker.new
 	end
 end
